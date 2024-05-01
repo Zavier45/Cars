c850c6fd5185423ac34fcedc348b40b5477d0b58
Zavier Hopson <zo.hopson94@gmail.com>
2024-04-30 19:00:42 -0500

    updated endpoints

--- scripts/Orders.js
+++ scripts/Orders.js
@@ -11,28 +11,18 @@ const interiors = await getInteriors();
 const techs = await getTechnologies();
 const wheels = await getWheels();
 
-export const Orders = () => {
-  const orders = getOrders();
+export const Orders = async () => {
+  const orders = await getOrders();
 
   return `${orders
     .map((order) => {
-      const paint = paints.find((p) => p.id === order.paintId);
-      const technology = techs.find((t) => t.id === order.technologyId);
-      const interior = interiors.find((i) => i.id === order.interiorId);
-      const wheel = wheels.find((w) => w.id === order.wheelId);
-
       return `<section class="order">
-                ${paint.color} car with
-                ${wheel.style} wheels,
-                ${interior.material} interior,
-                and the ${technology.package}
+                ${order.paint.color} car with
+                ${order.wheel.style} wheels,
+                ${order.interior.material} interior,
+                and the ${order.technology.package}
                 for a total cost of
-                ${(
-                  paint.price +
-                  technology.price +
-                  interior.price +
-                  wheel.price
-                ).toLocaleString("en-US", {
+                ${order.totalCost.toLocaleString("en-US", {
                   style: "currency",
                   currency: "USD",
                 })}
